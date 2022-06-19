92620d9f4dc28c6067a8cb836453c3a58c38d13b
Xavier Mas <xvrmasgarcia@gmail.com>
2022-06-19 15:37:26 +0200

    joke

--- joke/src/main.js
+++ joke/src/main.js
@@ -1,5 +1,15 @@
 
+let rateSave = [];
 
+function aleatori() {
+    let numero = Math.ceil(Math.random() * 100)
+    console.log(numero)
+    if (numero % 2 == 0) {
+        getJoke();
+    } else {
+        getJokeNorris();
+    }
+}
 
 async function getJoke() {
     const response = await fetch(` https://icanhazdadjoke.com/`, {
@@ -8,7 +18,7 @@ async function getJoke() {
         }
     });
     const joke = await response.json();
-    console.log(joke);
+    console.log(joke.joke)
     document.getElementById(`app`).innerHTML = joke.joke;
 }
 
@@ -18,10 +28,22 @@ async function getJokeNorris() {
     const jokeNorris = await response.json();
     console.log(jokeNorris.value)
     document.getElementById(`app`).innerHTML = jokeNorris.value;
-
 }
 
+class Rating {
+    constructor(joke, score, date) {
+        this.joke = joke;
+        this.score = score;
+        this.date = date;
+    }
+}
 
-const d = new Date();
-let text = d.toISOString();
-console.log(text)
+function reportAcudits(score) {
+    let joke = document.getElementById(`app`).textContent;
+    const d = new Date();
+    let date = d.toISOString();
+    let rate = new Rating(joke, score, date)
+    rateSave.push(rate)
+    console.log(rateSave)
+    alert(`Gracies per votar`)
+}
