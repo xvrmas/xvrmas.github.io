4a23776e428b65279e8a1795564ab30a90e16ee4
Xavier Mas <xvrmasgarcia@gmail.com>
2024-01-07 09:51:18 +0100

    create columns and javascript link

---
+++ generixhelp/script/main.js
@@ -0,0 +1,13 @@
+function hola()
+{
+    document.getElementById("2").innerText = "Hola";
+}function hola2()
+{
+    document.getElementById("2").innerText = "Hola 2";
+}function hola3()
+{
+    document.getElementById("2").innerText = "Hola 3";
+}function hola4()
+{
+    document.getElementById("2").innerText = "Hola 4";
+}
