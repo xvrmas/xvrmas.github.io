505782dea921605e95575fec342e9e8e17d89a17
Xavier Mas <xvrmasgarcia@gmail.com>
2024-01-08 12:12:31 +0100

    added new info

--- generixhelp/script/main.js
+++ generixhelp/script/main.js
@@ -1,23 +1,25 @@
 
 function mostrar(obj) {
-    div = document.getElementById(obj);
-    div.style.display = '';
+    let diva = document.getElementById(obj);
+    diva.style.display = '';
 }
 
 function reset() {
-    var divs = document.getElementsByClassName('divi');
+    let divs = document.getElementsByClassName('divi');
     for (var i = 0; i < divs.length; i++) {
         divs[i].style.display = 'none';
     }
 }
-
+function    alerta(msg)
+{
+    alert(msg);
+}
 let botones = document.querySelectorAll('.boton');
 
 function cambiarColor(botonSeleccionado) {
     botones.forEach(boton => {
         boton.classList.remove('boton-seleccionado');
     });
-
     botonSeleccionado.classList.add('boton-seleccionado');
 }
 
