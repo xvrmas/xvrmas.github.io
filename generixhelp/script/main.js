11b41a6ba275257e78ac0fbfa73fc187250bc776
Xavier Mas <xvrmasgarcia@gmail.com>
2024-01-08 10:52:52 +0100

    added new info

--- generixhelp/script/main.js
+++ generixhelp/script/main.js
@@ -1,43 +1,16 @@
-/*
-function hola(num)
-{
-    document.getElementById("obj1").innerText = num;
-}function hola2(num)
-{
-    document.getElementById("obj2").innerText = num;
-}function hola3(num)
-{
-    document.getElementById("obj3").innerText = num;
-}function hola4(num)
-{
-    document.getElementById("obj4").innerText = num;
-}*/
 
-function mostrar1() {
-    div = document.getElementById('obj1');
-    div.style.display = '';
-}
-function mostrar2() {
-    div = document.getElementById('obj2');
-    div.style.display = '';
-} function mostrar3() {
-    div = document.getElementById('obj3');
-    div.style.display = '';
-} function mostrar4() {
-    div = document.getElementById('obj4');
+function mostrar(obj) {
+    div = document.getElementById(obj);
     div.style.display = '';
 }
 
-function alerta() {
+function reset() {
     var divs = document.getElementsByClassName('divi');
     for (var i = 0; i < divs.length; i++) {
         divs[i].style.display = 'none';
     }
 }
-function alerta1()
-{
-    alert('como saber el contenido de una etiqueta')
-}
+
 let botones = document.querySelectorAll('.boton');
 
 function cambiarColor(botonSeleccionado) {
