e55747950f5878868ff783a4d99a673c752bf4c8
Xavier Mas <xvrmasgarcia@gmail.com>
2024-01-14 14:58:27 +0100

    added download file

--- generixhelp/script/main.js
+++ generixhelp/script/main.js
@@ -15,22 +15,30 @@ function alerta(msg) {
     alert(msg);
 }
 
-function cambiarColor(botonSeleccionado) {
-    let botones = document.querySelectorAll('.boton');
-    botones.forEach(boton => {
-        boton.classList.remove('boton-seleccionado');
-    });
-    botonSeleccionado.classList.add('boton-seleccionado');
-}
-// Obtener todas las imágenes con la clase "imagen-ampliable"
-var imagenesAmpliables = document.querySelectorAll('.imagen-ampliable');
+// Línea 1: agafa tots el elements de la clase boto i els guarda a la variable botons
+// Línea 2: Itera sobre tots els elements que em guardat de la clase 'boto'.
+// Línea 3: Remou la clase 'boto-seleccionat' de css de cada botó.
+// Línea 4: Afegeix la clase 'boto-seleccionat' al botó que em pasat com  argument.
 
-// Agregar un evento de clic a cada imagen
-imagenesAmpliables.forEach(function(imagen) {
-    imagen.addEventListener('click', function() {
+function cambiarColor(botoSeleccionat) {
+    let botons = document.querySelectorAll('.boto');
+    botons.forEach(boton => {
+        boton.classList.remove('boto-seleccionat');
+    });
+    botoSeleccionat.classList.add('boto-seleccionat');
+}
+
+// guardem a la variable "imatgesAmpliables" la clase "imatge-ampliable"
+var imatgesAmpliables = document.querySelectorAll('.imagte-ampliable');
+
+// Afegir un event de clic a cada imatgen
+imatgesAmpliables.forEach(function (imatge) {
+    imatge.addEventListener('click', function () {
         this.classList.toggle('ampliada');
     });
 });
+
+
 
 
 
