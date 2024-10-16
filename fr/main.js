1988168ece9479ce9266afaf6f0386650dbaafb9
Xavier Mas Garcia <xvrmasgarcia@gmail.com>
2024-10-16 20:40:27 +0200

    afeigida carpeta fr

---
+++ fr/main.js
@@ -0,0 +1,153 @@
+
+
+
+const paletsReferencia =
+    [
+        {
+            model: 920007,
+            quantitatMinima: 240,
+            midaReferencia: 'ample',
+        },
+        {
+            model: 920107,
+            quantitatMinima: 150,
+            midaReferencia: 'ample',
+        },
+        {
+            model: 920146,
+            quantitatMinima: 480,
+            midaReferencia: 'ample',
+        }, {
+            model: 920243,
+            quantitatMinima: 480,
+            midaReferencia: 'ample',
+        }, {
+            model: 920244,
+            quantitatMinima: 240,
+            midaReferencia: 'ample',
+        }, {
+            model: 920245,
+            quantitatMinima: 240,
+            midaReferencia: 'estret',
+        }, {
+            model: 920299,
+            quantitatMinima: 220,
+            midaReferencia: 'ample',
+        }, {
+            model: 920342,
+            quantitatMinima: 240,
+            midaReferencia: 'ample',
+        }, {
+            model: 920351,
+            quantitatMinima: 396,
+            midaReferencia: 'ample',
+        }, {
+            model: 920352,
+            quantitatMinima: 120,
+            midaReferencia: 'estret',
+        }, {
+            model: 920353,
+            quantitatMinima: 240,
+            midaReferencia: 'ample',
+        }, {
+            model: 920354,
+            quantitatMinima: 240,
+            midaReferencia: 'ample',
+        }, {
+            model: 920355,
+            quantitatMinima: 120,
+            midaReferencia: 'estret',
+        }, {
+            model: 920359,
+            quantitatMinima: 240,
+            midaReferencia: 'ample',
+        }, {
+            model: 920361,
+            quantitatMinima: 240,
+            midaReferencia: 'estret',
+        }, {
+            model: 920366,
+            quantitatMinima: 240,
+            midaReferencia: 'estret',
+        }, {
+            model: 920442,
+            quantitatMinima: 396,
+            midaReferencia: 'ample',
+        }, {
+            model: 920393,
+            quantitatMinima: 396,
+            midaReferencia: 'ample',
+        }, {
+            model: 920394,
+            quantitatMinima: 396,
+            midaReferencia: 'ample',
+        }, {
+            model: 920396,
+            quantitatMinima: 396,
+            midaReferencia: 'ample',
+        }, {
+            model: 920614,
+            quantitatMinima: 120,
+            midaReferencia: 'ample',
+        }, {
+            model: 920111,
+            quantitatMinima: 120,
+            midaReferencia: 'estret',
+        }, {
+            model: 920372,
+            quantitatMinima: 396,
+            midaReferencia: 'ample',
+        }, {
+            model: 920380,
+            quantitatMinima: 240,
+            midaReferencia: 'ample',
+        }, {
+            model: 920427,
+            quantitatMinima: 240,
+            midaReferencia: 'ample',
+        }, {
+            model: 920618,
+            quantitatMinima: 396,
+            midaReferencia: 'estret',
+        }, {
+            model: 920430,
+            quantitatMinima: 396,
+            midaReferencia: 'ample',
+        }, {
+            model: 920438,
+            quantitatMinima: 240,
+            midaReferencia: 'ample',
+        }, {
+            model: 920439,
+            quantitatMinima: 396,
+            midaReferencia: 'ample',
+        }, {
+            model: 920449,
+            quantitatMinima: 240,
+            midaReferencia: 'estret',
+        }
+    ]
+
+
+
+
+function resumPalets() {
+    let referencia = document.getElementById('referencia').value;
+    let mida = parseInt(document.getElementById('quantitat').value);
+    console.log(mida)
+    let result = paletsReferencia.filter(element => element.model == referencia);
+    if (result.length == 0 || isNaN(mida)) {
+        alert('falten dades!')
+    }
+    else {
+        document.getElementById('minim').innerHTML = 'Quantitat per palet: ' + result[0].quantitatMinima;
+        document.getElementById('demanat').innerHTML = 'Quantitat demanada: ' + mida;
+        if ((mida / result[0].quantitatMinima) == 1) {
+            document.getElementById('palets').innerHTML = 'Total ' + (mida / result[0].quantitatMinima) + ' palet ' + result[0].midaReferencia;
+
+        }
+        else {
+            document.getElementById('palets').innerHTML = 'Total ' + (mida / result[0].quantitatMinima) + ' palets ' + result[0].midaReferencia + 's';
+        }
+    }
+}
