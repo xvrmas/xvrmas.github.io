77b74db5289037cd2003a6b60379471789877565
Xavier Mas Garcia <xvrmasgarcia@gmail.com>
2025-01-27 18:32:00 +0100

    maquetacio taula

--- fr/main.js
+++ fr/main.js
@@ -190,82 +190,71 @@ var suma = [];
 let total = document.querySelector('.total')
 
 
-function recontePalets(suma)
-{
+function recontePalets(suma) {
     let i = 0;
     let suma2 = 0;
-    while (i < suma.length) 
-    {
+    while (i < suma.length) {
         suma2 += suma[i]
         i++;
     }
     total.innerHTML = `${suma2} palets`;
 }
 
-function resumPalets() 
-{
+function resumPalets() {
 
     let referencia = document.getElementById('referencia').value;
-    let demanat = parseInt(document.getElementById('quantitat').value);
+    let demanat = document.getElementById('quantitat').value;
     let result = paletsReferencia.filter(element => element.model == referencia);
 
-    if (result.length == 0 || isNaN(demanat)) 
-    {
+    if (result.length == 0 || isNaN(demanat)) {
         alert('falten dades o son incorrectes!')
     }
-    else 
-    {
+    else {
 
         let totalPalets = parseInt(demanat / result[0].quantitatMinima);
         suma.push(totalPalets)
-        const mostraInfo = document.querySelector('.contenidorInfo')
-        const contenidorPalets = document.createElement('table');
-        contenidorPalets.classList.add('taula')
-        contenidorPalets.innerHTML =
-            `  
-                <table class="contenidorInfo">
-                    <tr>
-                        <td class="mida">${result[0].midaReferencia}</td>
-                        <td class="carrer">${result[0].carrer}</td>
-                        <td class="Bloc">${result[0].bloc}</td>
-                        <td class="quantitatPalet">(${result[0].quantitatMinima})</td>
-                        <td class="quantitaDemanada">${demanat}</td>
-                        <td class="codiArticle">${result[0].model}</td>
-                        <td class="totalPalets">${totalPalets}</td>
-                        <td class="NOF"></td>
-                        <td class="creuTancar" onclick="eliminar(this)">Eliminar</td> 
-                    </tr>
-                </table>
-            `
-        mostraInfo.append(contenidorPalets)
+
+        const taulaDinamicaBody = document.querySelector('.taulaDinamica tbody');
+        const fila = document.createElement('tr');
+
+        fila.innerHTML = `
+            <td class="mida">${result[0].midaReferencia}</td>
+            <td class="carrer">${result[0].carrer}</td>
+            <td class="bloc">${result[0].bloc}</td>
+            <td class="quantitatPalet">(${result[0].quantitatMinima})</td>
+            <td class="quantitaDemanada">${demanat}</td>
+            <td class="totalPalets">${totalPalets}</td>
+            <td class="codiArticle">${result[0].model}</td>
+            <td class="creuTancar" onclick="eliminar(this)">Eliminar</td>
+            <td class="nof"></td>
+             
+        `;
+        taulaDinamicaBody.appendChild(fila);
+        recontePalets(suma);
     }
-    recontePalets(suma);
+
 }
 
 
-function eliminar(fila) 
-{
-        let linea = fila.closest('tr');       
-        let quantitatEliminada = parseInt(linea.querySelector('.totalPalets').innerText);
-        let coditEliminat = linea.querySelector('.codiArticle').innerText;
-        if (quantitatEliminada == 1)
-        {
-            var pregunta = confirm(`vols eliminar ${quantitatEliminada} palet del codi ${coditEliminat}?`)            
-        }
-        else
-        {
-            pregunta = confirm(`vols eliminar ${quantitatEliminada} palets del codi ${coditEliminat}?`)
-        }
-        if (pregunta == true) 
-        {
-                suma.splice(suma.indexOf(quantitatEliminada), 1);
-                linea.parentNode.removeChild(linea);
-        
-                recontePalets(suma); 
-        }      
-         
+
+function eliminar(fila) {
+    let linea = fila.closest('tr');
+    let quantitatEliminada = parseInt(linea.querySelector('.totalPalets').innerText);
+    let codiEliminat = linea.querySelector('.codiArticle').innerText;
+    if (quantitatEliminada == 1) {
+        var pregunta = confirm(`vols eliminar ${quantitatEliminada} palet del codi ${codiEliminat}?`)
+    }
+    else {
+        pregunta = confirm(`vols eliminar ${quantitatEliminada} palets del codi ${codiEliminat}?`)
+    }
+    if (pregunta == true) {
+        suma.splice(suma.indexOf(quantitatEliminada), 1);
+        linea.parentNode.removeChild(linea);
+        recontePalets(suma);
+    }
+
 }
-    
+
 
 
 
