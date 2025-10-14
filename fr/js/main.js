b607ae80e597edad836a85a49e14fea22f2e52d5
Xavier Mas Garcia <xvrmasgarcia@gmail.com>
2025-10-14 17:55:43 +0200

    cambi a text area fr

--- fr/js/main.js
+++ fr/js/main.js
@@ -157,7 +157,7 @@ function pinta(quantitatDemanada, infoPreparacio, paletsPreparar)
             <td class="quantitatDemanada">${quantitatDemanada}</td>
             <td class="codiArticle">${infoPreparacio.model}</td>
             <td class="paletsPreparar">${paletsPreparar}</td>
-            <td><textarea id="comentaris" name="comentaris" rows="2" cols="13"></textarea></td>
+            <td><textarea id="comentaris" name="comentaris" rows="1" cols="13"></textarea></td>
             <td class="boto"><button class="btn-eliminar" data-id="1">ELIMINAR</button></td>
 
     `;
