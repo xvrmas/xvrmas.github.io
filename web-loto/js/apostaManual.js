96a18c43779d9def27d7ebd17c177dd4e7758a29
Xavier Mas Garcia <xvrmasgarcia@gmail.com>
2025-11-18 21:31:06 +0100

    afegit modal

--- web-loto/js/apostaManual.js
+++ web-loto/js/apostaManual.js
@@ -1,9 +1,42 @@
 import {SALDO,periode,saldoLotoBolsa} from './main.js';
 
+function omplirAposta()
+{
+    let text = 'aqui van les caselles';
+    return text;
+}
+
 export function apostar()
 {
-    SALDO.saldo += 2;
-    alert(`saldo: ${SALDO.saldo}\nperiode: ` + periode )
-    saldoLotoBolsa.innerHTML = `Saldo: ${SALDO.saldo}€`;
+    let contingut = omplirAposta();
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let contingutModal = document.createElement('div');
+    contingutModal.classList.add('contingut-modal');
+   
+    let nodeText = document.createElement('p');
+    nodeText.textContent = contingut;
+    contingutModal.appendChild(nodeText)
+
+    let botoTancament = document.createElement('span');
+    botoTancament.innerHTML = '&times;';
+    botoTancament.classList.add('tanca');
+
     
+    botoTancament.onclick = function ()
+    {
+        modal.style.display = 'none';
+    };
+
+    contingutModal.appendChild(botoTancament);
+    modal.appendChild(contingutModal);
+
+    document.body.appendChild(modal);
+
+
+    /*SALDO.saldo += 2;
+    saldoLotoBolsa.innerHTML = `Saldo: ${SALDO.saldo}€`;
+    alert(`saldo: ${SALDO.saldo}\nperiode: ` + periode );*/
 }
