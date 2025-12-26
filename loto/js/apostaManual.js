0fe8417524ae25b0d8e05c0fa4558b9a95215496
Xavier Mas Garcia <xvrmasgarcia@gmail.com>
2025-12-26 09:54:06 +0100

    loto

--- loto/js/apostaManual.js
+++ loto/js/apostaManual.js
@@ -1,42 +1,38 @@
-//pint la cuadricula del 1 al 49 i permet sel·leccionar 6 numeros 
+//pinta la cuadricula del 1 al 49 i permet sel·leccionar 6 numeros i només 6
+
 
 import { ESTAT, desarEstat, carregarEstat } from './estat.js';
 
 document.addEventListener('DOMContentLoaded', () =>
 {
-
   carregarEstat();
 
   let seleccioActual = [];
-
   const contenidor = document.getElementById('selector-numeros');
-  const seleccioPinta = document.getElementById('seleccio-actual');
+  const seleccioEl = document.getElementById('seleccio-actual');
   const botoJugar = document.getElementById('boto-jugar');
 
-
-  for (let i = 1; i <= 49; i++) 
+  for (let i = 1; i <= 49; i++)
   {
-
     const btn = document.createElement('button');
     btn.textContent = i;
     btn.classList.add('numero-btn');
 
-    btn.addEventListener('click', () => 
+    btn.addEventListener('click', () =>
     {
-      if (seleccioActual.includes(i)) 
+      if (seleccioActual.includes(i))
       {
         seleccioActual = seleccioActual.filter(n => n !== i);
         btn.classList.remove('seleccionado');
-      }
-      else if (seleccioActual.length < 6) 
+      } else if (seleccioActual.length < 6)
       {
         seleccioActual.push(i);
         btn.classList.add('seleccionado');
       }
 
-      seleccioPinta.textContent = `Selected: ${seleccioActual.join(' - ')}`;
+      seleccioEl.textContent = `Selected: ${seleccioActual.join(', ')}`;
 
-      contenidor.querySelectorAll('button').forEach(b => 
+      contenidor.querySelectorAll('button').forEach(b =>
       {
         if (!seleccioActual.includes(Number(b.textContent)))
         {
@@ -48,21 +44,15 @@ document.addEventListener('DOMContentLoaded', () =>
     contenidor.appendChild(btn);
   }
 
-  botoJugar.addEventListener('click', () => 
+  botoJugar.addEventListener('click', () =>
   {
-    const numeroSorteigs = parseInt(document.getElementById('num-sorteigs').value)
     if (seleccioActual.length !== 6)
     {
       alert('You must select exactly 6 numbers');
       return;
     }
-    if (isNaN(numeroSorteigs) || numeroSorteigs < 1)
-    {
-      alert('Please enter a valid number of draws');
-    }
-    ESTAT.apostaActual = [...seleccioActual];
-    ESTAT.numApostes = numeroSorteigs;
 
+    ESTAT.apostaActual = [...seleccioActual];
     desarEstat();
     window.location.href = './resultado.html';
   });
