af3600daddcff4d1e2176918130fd4fa79515c02
Xavier Mas <96140291+xvrmas@users.noreply.github.com>
2026-02-06 07:35:14 +0100

    Actualizar apostaManual.js

--- loto/js/apostaManual.js
+++ loto/js/apostaManual.js
@@ -50,7 +50,7 @@ document.addEventListener('DOMContentLoaded', () =>
       const creaP = document.createElement('p'); // CREAR AQUÍ DINS
       creaP.innerHTML =
         `<div id="seleccio-actual">
-                 Block ${index + 1}:<br> ${element.join('-')}
+                 Block ${index + 1}:<br><span class="ball"> ${element.join('-')}</span>
             </div>
             `;
       mostrarNumeros.appendChild(creaP);
