b502f548c70833614a98ac91ba24a4a18a5477ed
Xavier Mas Garcia <xvrmasgarcia@gmail.com>
2026-06-02 18:49:48 +0200

    primer commit la nostra

---
+++ lanostra/js/main.js
@@ -0,0 +1,13 @@
+const elements = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+    }
+  });
+}, {
+  threshold: 0.15
+});
+
+elements.forEach(el => observer.observe(el));
